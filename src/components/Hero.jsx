3788580a3eba6be240cab82f9af0940bb5845336
Ahortu901 <aricktech.edu@gmail.com>
2023-10-16 23:09:20 +0100

    adding more

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,12 +3,23 @@ import { logo } from '../assets';
 function Hero() {
   return (
     <header className='w-full flex justify-center items-center flex-col'>
-      <nav className="flex justify-between items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
         <img src={logo} alt='sumz_logo' className="w-28 object-contain"/>
-        <button type="button" onClick={() => window.open('https//')}>
+        <button type="button" onClick={() => window.open('https://github.com/Ahortu901/summarizer')} className="black_btn">
           GitHub
         </button>
       </nav>
+
+      <h1 className="head_text">
+        Summarize Articles with <br className="max-md:hidden"/>
+        <span className='orange_gradient'>OpenAI GPT-4</span>
+      </h1>
+      <h2 className="desc">
+        Simplify your reading with Summarize, an 
+        open-source article summarizer
+        that transforms lengthy article into
+        clear and concise suuaries
+      </h2>
     </header>
   )
 }
